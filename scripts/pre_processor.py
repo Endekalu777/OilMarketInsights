fe80a184006f62f8edad8daa8f869e671326ad52
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-10-30 19:45:37 +0300

    feat: Implement logging and data loading functionality
    - Imported logging module and configured basic logging settings to display INFO level messages.
    - Added load_data method to read Brent oil prices from a CSV file.
    - Parsed 'Date' column as datetime with dayfirst format.
    - Stripped whitespace from column names and sorted DataFrame by 'Date'.
    - Logged an informational message upon successful data loading and sorting.

--- scripts/pre_processor.py
+++ scripts/pre_processor.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 class PreProcessor():
+    # Initialize the Preprocessor with a path to the CSV data file.
     def __init__(self, filepath):
         self.data_path = filepath
         self.df = None
+        
+
+    # Load Brent oil prices data from the CSV file.
+    def load_data(self):
+        self.df = pd.read_csv(self.data_path, parse_dates = ['Date'], dayfirst = True)
+        self.date.columns = self.date.columns.str.strip()
+        self.date.sort_values('Date', inplace = True)
+        logging.info("Data loaded and sorted by date")
+        return self.data
 
