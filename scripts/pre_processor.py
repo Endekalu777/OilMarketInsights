0c23ed9ace83bade6d95a2ccd9905855871d85fe
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-10-30 22:28:06 +0300

    feat: Enhance PreProcessor with logging and error handling
    - Added logging configuration to log messages to a file and console.
    - Created logs directory if it doesn't exist.
    - Updated __init__ method to log initialization with the provided file path.
    - Implemented error handling in load_data method to log loading errors.
    - checked the functionality of the script by running in the
      pre_processor notebook file.

--- scripts/pre_processor.py
+++ scripts/pre_processor.py
@@ -1,20 +1,37 @@
 import pandas as pd
 import logging
+from IPython.display import display
+import os
 
+# Create logs directory if it doesn't exist
+if not os.path.exists('../logs'):
+    os.makedirs('../logs')
 logging.basicConfig(level=logging.INFO,
+                    handlers = [logging.FileHandler('../logs/pre_process.log')],
                     format='%(asctime)s:%(levelname)s:%(message)s')
+
+
 
 class PreProcessor():
     # Initialize the Preprocessor with a path to the CSV data file.
     def __init__(self, filepath):
         self.data_path = filepath
         self.df = None
+        logging.info("Preprocessor initialized with file: {filepath}")
         
 
     # Load Brent oil prices data from the CSV file.
     def load_data(self):
-        self.df = pd.read_csv(self.data_path, parse_dates = ['Date'], dayfirst = True)
-        self.df.columns = self.df.columns.str.strip()
-        self.df.sort_values('Date', inplace = True)
-        logging.info("Data loaded and sorted by date")
-        return self.df
+        try:
+            self.df = pd.read_csv(self.data_path, parse_dates = ['Date'], dayfirst = True)
+            self.df.columns = self.df.columns.str.strip()
+            self.df.sort_values('Date', inplace = True)
+            logging.info("Data loaded and sorted by date")
+            return self.df
+        except Exception as e:
+            logging.error(f"Error loading data: {str(e)}")
+            raise
+    
+    # Handle missing values and perform initial data cleaning.
+    def pre_process_data(self):
+        display(self.df.isnull().sum())
