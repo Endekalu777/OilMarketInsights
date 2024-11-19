9bbfde8f5a7378c26c87a86e38161b054b0a8842
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-11-19 19:56:44 +0300

    feat: Enhance Flask API with indicators and prediction endpoint
    - Loaded Brent oil indicators from CSV for integration into the API.
    - Added new API endpoint `/api/indicators` to retrieve historical Brent oil indicators in JSON format.
    - Implemented a `/api/predict` POST endpoint to accept features for prediction using the pre-trained LSTM model:
      - The endpoint processes incoming JSON data, reshapes it for model input, and returns the prediction result.
    - Integrated the loading of the LSTM model from a saved file, ensuring that predictions can be made based on the trained model.

--- oil_price_dashboard/backend/app.py
+++ oil_price_dashboard/backend/app.py
@@ -1,11 +1,17 @@
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from tensorflow.keras.models import load_model
+import numpy as np
 
 app = Flask(__name__)
 
 # Load the data
 historical_prices = pd.read_csv("../data/BrentOilPrices.csv")
 forecast = pd.read_csv("../data/lstm_forecasted_prices.csv")
+brent_indicators = pd.read_csv('data/Brent_oil_indicators.csv')
+
+# Load LSTM model
+lstm_model = load_model('data/lstm_model.h5')
 
 # Convert DataFrames to JSON-serializable format (list of dictionaries)
 historical_prices_json = historical_prices.to_dict(orient='records')
@@ -18,6 +24,16 @@ def get_historical_prices():
 @app.route('/api/forecast', methods=['GET'])
 def get_forecast():
     return jsonify(forecast_json)
+
+@app.route('/api/indicators', methods=['GET'])
+def get_indicators():
+    return jsonify(brent_indicators.to_dict(orient='records'))
+
+@app.route('/api/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+    prediction = lstm_model.predict(np.array(data).reshape(1, -1)).tolist()
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
